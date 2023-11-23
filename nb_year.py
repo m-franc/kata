c6924c34e035f039fd4cf4c8afbe5da87cdfda50
mrchoung <maxime.franc1@gmail.com>
2023-11-23 16:05:45 +0100

    first kata of the year, whith while loop ! Long time no see

---
+++ nb_year.py
@@ -0,0 +1,10 @@
+def nb_year(p0, percent, aug, p):
+    
+    years = 0
+    while p0 < p:
+        print("coucou")
+        p0 += p0 * (percent / 100) + aug
+        years += 1
+    return years
+
+print(nb_year(1500000, 2.5, 10000, 2000000))
