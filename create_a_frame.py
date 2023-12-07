ce29813ae26e381ad20308d3e5ac360f33782249
mrchoung <maxime.franc1@gmail.com>
2023-12-07 20:49:34 +0100

    kata of the day ! Of yesterday... Interesting, some generator notion w yield see in correction. And ljust method.

--- create_a_frame.py
+++ create_a_frame.py
@@ -1,41 +1,19 @@
 #https://www.codewars.com/kata/5672f4e3404d0609ec00000a/train/python
 
 
-def fill_word(word, elem):
-    #print(f"le fruit : {word} et la ligne du tableau : {elem}")
-    new_elem = elem
-    i_e = 0
-    i_w = 0
-    while i_e < len(elem):
-        while i_e == 1:
-            new_elem[i_e] = ' '
-            i_e += 1
-        while i_e >= 2 and i_w < len(word):
-            new_elem[i_e] = word[i_w]
-            i_w += 1
-            i_e += 1
-        i_e += 1
-    i_e = len(word) + 2
-    while i_e < len(elem) - 1:
-        new_elem[i_e] = ' '
-        i_e += 1
-    return new_elem
+def fill_mid_line(text, char, width):
+    mid_line = []
+    for elem in text:
+        elem = char + ' ' + elem + (' ' * (width - len(elem) - 3)) + char
+        mid_line.append(elem)
+    return mid_line
 
-
-def create_frame(text, char):
+def frame(text, char):
     width = len(max(text, key=len)) + 4
-    #print(width)
-    frame = [[char] * width] * (len(text) + 2)
-    #print(frame)
-    i_t = 0
+    frame = fill_mid_line(text, char, width) 
+    frame.insert(0, char*width)
+    frame.insert(len(frame), char*width)
+    frame_rdy_to_str = list(map(lambda elem: elem + '\n', frame))
+    return "".join(frame_rdy_to_str)[:-1]
 
-    for i_f, elem in enumerate(frame, 1):
-        if i_t == len(text):
-            break
-        frame[i_f] = fill_word(text[i_t], frame[i_f])
-        #print(frame)
-        i_t += 1
-    print(frame[0])
-    return 0
-
-print(create_frame(['apple', 'banana', 'watermelon', 'orange'], "@"))
+print(frame(['apple', 'banana', 'watermelon', 'orange'], "@"))
