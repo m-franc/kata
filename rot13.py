63a151e59fe597b656fa01e8198063ce918f5a99
mrchoung <maxime.franc1@gmail.com>
2023-12-21 17:46:44 +0100

    kate of the day ! Next level yeeee. About rot13. Some cool tricks with index method

---
+++ rot13.py
@@ -0,0 +1,21 @@
+from string import ascii_uppercase as alph_up
+from string import ascii_lowercase as alph_lw
+
+def rot13(message):
+    
+    new_message = ""
+    for c in message:
+        if c.isalpha():
+            if c.isupper():
+                rot13_i = alph_up.index(c)+13 if alph_up.index(c)+13 < 26 else alph_up.index(c)+13-26
+                new_message += alph_up[rot13_i]
+            elif c.islower():
+                rot13_i = alph_lw.index(c)+13 if alph_lw.index(c)+13 < 26 else alph_lw.index(c)+13-26
+                new_message += alph_lw[rot13_i]
+        else:
+            new_message += c
+    return new_message
+
+print({k : v for v, k in enumerate(alph_up, 1)})
+
+print(rot13("TeSt"))
