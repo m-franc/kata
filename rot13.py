367a292fe7ae8028be81871ad08bbae92fa98f39
mrchoung <maxime.franc1@gmail.com>
2023-12-21 17:55:51 +0100

    little modif on the kata of the day hehe

--- rot13.py
+++ rot13.py
@@ -7,10 +7,10 @@ def rot13(message):
     for c in message:
         if c.isalpha():
             if c.isupper():
-                rot13_i = alph_up.index(c)+13 if alph_up.index(c)+13 < 26 else alph_up.index(c)+13-26
+                rot13_i = alph_up.index(c)+13 if alph_up.index(c)+13 < 26 else alph_up.index(c)-13
                 new_message += alph_up[rot13_i]
             elif c.islower():
-                rot13_i = alph_lw.index(c)+13 if alph_lw.index(c)+13 < 26 else alph_lw.index(c)+13-26
+                rot13_i = alph_lw.index(c)+13 if alph_lw.index(c)+13 < 26 else alph_lw.index(c)-13
                 new_message += alph_lw[rot13_i]
         else:
             new_message += c
