ad3c1e97cfcef9cfb19a7f2b7c02b555fe5e5f50
mrchoung <maxime.franc1@gmail.com>
2024-01-03 14:50:39 +0100

    kata of the day ! Interesting, easier with rstrip, honneslty. And some cool re tricks see in correction, we progress in re, its important

--- remove.py
+++ remove.py
@@ -1,17 +1,20 @@
 #https://www.codewars.com/kata/57faf32df815ebd49e000117/train/python
 
+import re
+
 def remove(st):
-    st_arr = st.split()
+    sts = st.split()
+    rev_sts = list(map(lambda x: x[::-1], sts))
     new_sts = []
-    for st in st_arr:
-        nb_mark = 0
-        st_rev = str(reversed(st))
-        print(st_rev)
-        for c in st_rev:
-            nb_mark += 1
-            if c != "!":
-                break
-        new_sts.append(st[:-nb_mark+1])
-    return new_sts
+    for st in rev_sts:
+        if st[0] == '!':
+            i = 0
+            while st[i] == '!':
+                i += 1
+            new_sts.append(st[i:])
+        else:
+            new_sts.append(st)
+    new_sts = list(map(lambda x: x[::-1], new_sts))
+    return "".join(new_sts)
 
 print(remove("!!!Hi !!hi!!! !hi"))
