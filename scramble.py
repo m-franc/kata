6c07fb7881e05c3d994adea8a0fb8e54d92d5297
mrchoung <maxime.franc1@gmail.com>
2024-01-15 21:49:50 +0100

    one of kata of last week done ! About counter here. Set difference learned in correction ! Very powerful

--- scramble.py
+++ scramble.py
@@ -1,12 +1,11 @@
 #https://www.codewars.com/kata/55c04b4cc56a697bb0000048/train/python
+from collections import Counter
 
 def scramble(s1, s2):
-    for c in s2:
-        if c in s1:
-            s1 = s1.replace(c, "", 1)
-        else:
-            return False
-    return True
+    s2 = Counter(s2)
+    for c in s1:
+        s2[c] -= 1
+    return all(cnt <= 0 for cnt in s2.values())
 
 
 print(scramble('scriptjava', 'javascript'))
