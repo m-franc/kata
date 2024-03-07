6bd7b2b30f3266873ec9bfdfd2847caa62d7a3b6
mrchoung <maxime.franc1@gmail.com>
2024-03-07 23:33:39 +0100

    kata of yesterday done ! I'm happy. Good to see, in correction, that tuple or set can use as a key in dictionnary... or use Counter as a key function in groupby... Great

--- group_anagrams.py
+++ group_anagrams.py
@@ -4,23 +4,41 @@ from collections import Counter
 from itertools import permutations
 
 def group_anagrams(words):
-    words_sorted = []
-    for word in words:
-        words_sorted.append("".join(sorted(word)))
-    words_sorted_cat = Counter(words_sorted)
+    words_sorted = list(map(sorted, words))
+    words_sorted = list(map("".join, words_sorted))
+    groups_words_sd = []
     groups_words = []
-    group_words = []
-    i = 0
-    for catword in words_sorted_cat.items():
-        print(catword[0])
-        all_anagrams = permutations(catword[0])
-        for o in range(catword[1]):
-            print(["".join(w) for w in all_anagrams])
-            for anagram in ["".join(w) for w in all_anagrams]:
-                group_words.append(words[i])
-                i += 1
-        groups_words.append(group_words)
-    print(groups_words)
-    return 0
+    for i, word_sd in enumerate(words_sorted):
+        group_word_sd = []
+        group_word = []
+        if i == 0:
+            group_word_sd.append(word_sd)
+            groups_words_sd.append(group_word_sd)
+            group_word.append(words[i])
+            groups_words.append(group_word)
+        else:
+            o = 0
+            while o < len(groups_words_sd):
+                if groups_words_sd[o][0] == word_sd:
+                    groups_words_sd[o].append(word_sd)
+                    groups_words[o].append(words[i])
+                    break
+                o += 1
+            if o == len(groups_words_sd):
+                groups_words_sd.append([word_sd])
+                groups_words.append([words[i]])
+    return groups_words
+
+def group_anagrams(words):
+    groups = dict()
+    for word in words:
+        key = tuple(sorted(word))
+        print(groups)
+        print(key)
+        try:
+            groups[key].append(word)
+        except:
+            groups[key] = [word]
+    return [v for v in groups.values()]
 
 print(group_anagrams(["tsar", "rat", "tar", "star", "tars", "cheese"]))
